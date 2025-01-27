db8980b6880158ee1ce1d6c9f540f524e339554c
Andre Luis Correa <andreluiscorrea2003@gmail.com>
2025-01-27 17:21:08 -0300

    feat: create bo classes

--- src/unioeste/geral/pessoa/bo/CPF.java
+++ src/unioeste/geral/pessoa/bo/CPF.java
@@ -3,4 +3,18 @@ package unioeste.geral.pessoa.bo;
 import java.io.Serializable;
 
 public class CPF implements Serializable {
+
+    private String cpf;
+
+    public CPF(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
--- src/unioeste/geral/pessoa/bo/Sexo.java
+++ src/unioeste/geral/pessoa/bo/Sexo.java
@@ -3,4 +3,28 @@ package unioeste.geral.pessoa.bo;
 import java.io.Serializable;
 
 public class Sexo implements Serializable {
+
+    private String sigla;
+    private String nome;
+
+    public Sexo(String sigla, String nome) {
+        this.sigla = sigla;
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
