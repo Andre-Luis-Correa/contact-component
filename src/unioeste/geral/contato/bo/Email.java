8551c2a328aaaf1c90901136c525f091df9ce4c8
Andre Luis Correa <andreluiscorrea2003@gmail.com>
2025-01-27 16:03:47 -0300

    feat: create bo classes

--- src/unioeste/geral/contato/bo/Email.java
+++ src/unioeste/geral/contato/bo/Email.java
@@ -5,4 +5,16 @@ import java.io.Serializable;
 public class Email implements Serializable {
 
     private String email;
+
+    public Email(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/unioeste/geral/contato/bo/Telefone.java
+++ src/unioeste/geral/contato/bo/Telefone.java
@@ -7,4 +7,34 @@ public class Telefone implements Serializable {
     private String numero;
     private DDD ddd;
     private DDI ddi;
+
+    public Telefone(String numero, DDD ddd, DDI ddi) {
+        this.numero = numero;
+        this.ddd = ddd;
+        this.ddi = ddi;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public DDD getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(DDD ddd) {
+        this.ddd = ddd;
+    }
+
+    public DDI getDdi() {
+        return ddi;
+    }
+
+    public void setDdi(DDI ddi) {
+        this.ddi = ddi;
+    }
 }
